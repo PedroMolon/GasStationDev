603de1e798836df8126c37306155763ffec18cb0
PedroMolon <pedroh.molonr@gmail.com>
2025-08-08 10:18:41 -0300

    first commit

---
+++ src/main/java/org/pedromolon/gasstationdev/GasStationDevApplication.java
@@ -0,0 +1,13 @@
+package org.pedromolon.gasstationdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasStationDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GasStationDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/pedromolon/gasstationdev/GasStationDevApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pedromolon.gasstationdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasStationDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
